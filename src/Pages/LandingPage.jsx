96437c1c1fe1490fb5bd82c9c657a896e805ca0b
Bima Saputra <sbima2432@gmail.com>
2023-07-03 23:09:23 +0700

    little refactoring landingpage

--- src/Pages/LandingPage.jsx
+++ src/Pages/LandingPage.jsx
@@ -7,11 +7,14 @@ import { RxHamburgerMenu } from "react-icons/rx";
 const LandingPage = () => {
   const [isBurger, setIsBurger] = useState(false);
   const navigate = useNavigate();
-  const mainRef = useRef(null);
-  const productRef = useRef(null);
-  const aboutRef = useRef(null);
-  const contactRef = useRef(null);
-  const footerRef = useRef(null);
+
+  const ref = {
+    mainRef: useRef(null),
+    productRef: useRef(null),
+    aboutRef: useRef(null),
+    contactRef: useRef(null),
+    footerRef: useRef(null),
+  };
 
   const handleScroll = (ref) => {
     window.scrollTo({
@@ -26,14 +29,14 @@ const LandingPage = () => {
       <Link
         to={"/#product"}
         onClick={() => {
-          handleScroll(mainRef.current);
+          handleScroll(ref.mainRef.current);
         }}
         className="fixed bottom-7 right-7 z-20"
       >
         <BsFillArrowUpCircleFill className=" rounded-full animate-bounce text-4xl text-blue-800 hover:text-blue-900  lg:text-blue-50 lg:hover:text-blue-200 cursor-pointer hover:shadow-md" />
       </Link>
       {/* navbar */}
-      <div className="w-full h-12 flex justify-center top-0 sticky bg-blue-50 text-slate-500 shadow-xl z-30">
+      <div className="w-full h-12 flex justify-center top-0 sticky bg-slate-50 text-slate-500 shadow-xl z-30">
         <div className="w-11/12 lg:w-4/6 flex justify-between items-center relative">
           <p className="text-2xl font-bold cursor-pointer hover:underline flex items-center hover:text-slate-700 transition-all ease-in-out">
             <img src={mbd} alt="Logo MBD" className="w-10 h-min mr-2" />
@@ -54,48 +57,36 @@ const LandingPage = () => {
                 (!isBurger ? "hidden lg:flex" : "")
               }
             >
-              <p className="cursor-pointer hover:underline hover:text-slate-700 transition-all ease-in-out mx-6 lg:mx-0">
-                <Link
-                  to={"/#product"}
-                  onClick={() => {
-                    setIsBurger(false);
-                    handleScroll(productRef.current);
-                  }}
-                >
-                  Product
-                </Link>
-              </p>
-              <p className="cursor-pointer hover:underline hover:text-slate-700 transition-all ease-in-out mx-6 lg:mx-0">
-                <Link
-                  to={"/#product"}
-                  onClick={() => {
-                    setIsBurger(false);
-                    handleScroll(aboutRef.current);
-                  }}
-                >
-                  About
-                </Link>
-              </p>
-              <p className="cursor-pointer hover:underline hover:text-slate-700 transition-all ease-in-out mx-6 lg:mx-0">
-                <Link
-                  to={"/#product"}
-                  onClick={() => {
-                    setIsBurger(false);
-                    handleScroll(contactRef.current);
-                  }}
-                >
-                  Contact
-                </Link>
-              </p>
-              <p
-                className="cursor-pointer hover:underline hover:text-slate-700 transition-all ease-in-out mx-6 lg:mx-0"
-                onClick={() => {
-                  setIsBurger(false);
-                  navigate("/signin");
-                }}
-              >
-                Login
-              </p>
+              {["Product", "About", "Contact", "Login"].map((e, idx) => {
+                return (
+                  <p
+                    key={"navbar-bar-" + idx}
+                    className="cursor-pointer hover:underline hover:text-slate-700 transition-all ease-in-out mx-6 lg:mx-0"
+                  >
+                    {e !== "Login" && (
+                      <Link
+                        to={`/#${e.toLowerCase()}`}
+                        onClick={() => {
+                          setIsBurger(false);
+                          handleScroll(ref[`${e.toLowerCase()}Ref`].current);
+                        }}
+                      >
+                        {e}
+                      </Link>
+                    )}
+                    {e === "Login" && (
+                      <p
+                        onClick={() => {
+                          setIsBurger(false);
+                          navigate("/signin");
+                        }}
+                      >
+                        {e}
+                      </p>
+                    )}
+                  </p>
+                );
+              })}
             </div>
           </div>
         </div>
@@ -103,7 +94,7 @@ const LandingPage = () => {
 
       {/* content 1 */}
       <div
-        ref={mainRef}
+        ref={ref.mainRef}
         className="w-full flex justify-center bg-gradient-to-tr bg-blue-600 -mt-20 lg:-mt-16 lg:h-screen text-blue-50 z-20 "
       >
         <div className="w-11/12 lg:w-4/6 flex flex-col lg:flex-row justify-between items-center">
@@ -129,7 +120,7 @@ const LandingPage = () => {
         </div>
       </div>
       <div
-        ref={productRef}
+        ref={ref.productRef}
         className="w-full flex justify-center mt-16 lg:-mt-5 bg-blue-600"
       >
         <div className="w-11/12 lg:w-4/6 h-screen flex justify-center bg-blue-50 rounded-2xl px-8 pt-20 z-10 lg:z-20">
@@ -137,7 +128,7 @@ const LandingPage = () => {
         </div>
       </div>
       <div
-        ref={aboutRef}
+        ref={ref.aboutRef}
         className="w-full flex justify-center bg-blue-600 mt-32"
       >
         <div className="w-11/12 lg:w-4/6 h-screen flex justify-center bg-blue-50 rounded-2xl px-8 pt-20 z-10">
@@ -145,7 +136,7 @@ const LandingPage = () => {
         </div>
       </div>
       <div
-        ref={contactRef}
+        ref={ref.contactRef}
         className="w-full flex justify-center bg-blue-600 mt-32"
       >
         <div className="w-11/12 lg:w-4/6 h-screen flex justify-center bg-blue-50 rounded-2xl px-8 pt-20 z-10">
@@ -153,7 +144,7 @@ const LandingPage = () => {
         </div>
       </div>
       <div
-        ref={footerRef}
+        ref={ref.footerRef}
         className="w-full flex justify-center bg-blue-600 mt-32"
       >
         <div className="w-11/12 lg:w-4/6 h-screen flex justify-center bg-blue-50 rounded-2xl px-8 pt-20 z-10">
