b622f5f177187cdd96b118eb69090a992046efa5
Bima Saputra <sbima2432@gmail.com>
2023-07-09 19:31:33 +0700

    fixing bug. Varcel, RollupError: Could not resolve "./app/store" from "src/main.jsx"

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import store from "./app/store";
+import Store from "./app/Store";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <Provider store={store}>
+  <Provider store={Store}>
     <BrowserRouter basename="/">
       <App />
     </BrowserRouter>
